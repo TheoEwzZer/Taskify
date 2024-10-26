7c38281bdc98db5f9ab5a9da6e628a09d9c76d0c
Théo <theo.fabianomattei@gmail.com>
2024-10-26 23:11:29 +0200

    refactor: refactor loading component to use PageLoader

--- src/app/(dashboard)/loading.tsx
+++ src/app/(dashboard)/loading.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-import { Loader } from "lucide-react";
+import { PageLoader } from "@/components/page-loader";
 import { ReactElement } from "react";
 
 const LoadingPage: () => ReactElement = () => {
-  return (
-    <div className="flex h-full flex-col items-center justify-center">
-      <Loader className="size-6 animate-spin text-muted-foreground" />
-    </div>
-  );
+  return <PageLoader />;
 };
 
 export default LoadingPage;
