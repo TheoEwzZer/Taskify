5f856167301ea6a8caaf4fee9b743c05ccd43b5e
Théo <theo.fabianomattei@gmail.com>
2024-11-24 13:48:41 +0100

    deps: update ESLint configuration and add TypeScript support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,66 @@
-import pluginJs from "@eslint/js";
-import prettierConfigRecommended from "eslint-plugin-prettier/recommended";
-import pluginReact from "eslint-plugin-react";
-import pluginReactHooks from "eslint-plugin-react-hooks";
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 import globals from "globals";
-import tseslint from "typescript-eslint";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-/** @type {import('eslint').Linter.Config[]} */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  prettierConfigRecommended,
+  ...fixupConfigRules(
+    compat.extends(
+      "eslint:recommended",
+      "next",
+      "next/core-web-vitals",
+      "next/typescript",
+      "plugin:@next/next/recommended",
+      "plugin:@tanstack/query/recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:react/recommended",
+      "prettier"
+    )
+  ),
   {
     plugins: {
-      "react-hooks": pluginReactHooks,
+      react: fixupPluginRules(react),
+      "@typescript-eslint": fixupPluginRules(typescriptEslint),
+      prettier,
+      "react-hooks": fixupPluginRules(reactHooks),
     },
-    settings: {
-      react: {
-        version: "detect",
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
       },
+
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
+
     rules: {
       indent: "off",
       quotes: ["error", "double"],
       semi: ["error", "always"],
       "eol-last": ["error", "always"],
-      "@typescript-eslint/no-require-imports": "off",
-      "@typescript-eslint/ban-types": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off", // Désactiver la règle problématique
       "react/prop-types": "off",
       "react/react-in-jsx-scope": "off",
-      "prettier/prettier": "off",
     },
-  },
-  {
-    ignores: [".next"],
   },
 ];
