d79a2f6d0eab88c21eb108d0684b68ac24a81fe0
Théo <theo.fabianomattei@gmail.com>
2024-12-19 15:53:16 +0100

    🐛 fix: update button text color for better visibility in TaskViewSwitcher

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -100,7 +100,7 @@ export const TaskViewSwitcher: ({
           </TabsList>
           <Button
             size="sm"
-            className="w-full lg:w-auto"
+            className="w-full text-white dark:text-black lg:w-auto"
             onClick={(): Promise<URLSearchParams> => open()}
           >
             <PlusIcon />
